4e4f032be13996890c0b169d7abe338f540b4157
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2021-09-04 21:50:10 +0530

    resolved refresh issue

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
   Link
 } from "react-router-dom";
 import { useState, useContext } from "react";
@@ -9,54 +10,56 @@ import './App.css';
 import Final from "./views/Final";
 import Home from "./views/Home";
 import SignIn from './views/SignIn';
-import AppContext from "./AppContext";
+// import AppContext from "./AppContext";
 import Game_1 from "./views/Game_1";
 import Game_2 from "./views/Game_2";
 import Game_3 from "./views/Game_3";
 import Game_4 from "./views/Game_4";
 import Game_5 from "./views/Game_5";
+import { AuthContext } from "./context/authContext";
 
 function App() {
-  const [user, setLoginUser] = useState();
-  const userSettings = {
-    user: user,
-    setLoginUser
-  }
-  const myContext = useContext(AppContext);
+  // const [user, setLoginUser] = useState();
+  // const userSettings = {
+  //   user: user,
+  //   setLoginUser
+  // }
+  const {user} = useContext(AuthContext);
   return (
-    <AppContext.Provider value={userSettings}>
+    <>
     <Router>
       <Switch>
-        
         <Route exact path = "/home">
-         {user? <Game_1/>:  <Home/>}
+         {user? <Redirect to="/game1"/>:  <Home/>}
         </Route>
         <Route exact path = "/game1" >
           {console.log(user)}
-          {user? <Game_1/>: <Home/>}
+            {user ? <Game_1 /> : <Redirect to="/home" />}
         </Route>
         <Route exact path = "/game2" >
-          {user? <Game_2/>: <Home/>}
+          {user? <Game_2/>: <Redirect to="/home" />}
         </Route>
         <Route exact path = "/game3" >
-          {user? <Game_3/>: <Home/>}
+          {user? <Game_3/>: <Redirect to="/home" />}
         </Route>
         <Route exact path = "/game4" >
-          {user? <Game_4/>: <Home/>}
+          {user? <Game_4/>: <Redirect to="/home" />}
         </Route>
         <Route exact path = "/game5" >
-          {user? <Game_5/>: <Home/>}
+          {user? <Game_5/>: <Redirect to="/home" />}
         </Route>
         <Route exact path = "/final" >
           {<Final/>}
         </Route>
         <Route exact path = "/signin">
-          { <SignIn/>}
-    
+            {user ? <Redirect to="/home" /> : <SignIn/>}
+        </Route>
+        <Route path="*">
+          <Redirect to="/signin"/>
         </Route>
       </Switch>
     </Router>
-  </AppContext.Provider>
+  </>
     
 
     
